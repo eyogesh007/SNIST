0460323ceeffe978d8f3873769b3b61bdc5778bb
eyogesh007 <81839475+eyogesh007@users.noreply.github.com>
2022-09-09 17:22:36 +0530

    html,js,css files

---
+++ index.js
@@ -0,0 +1,22 @@
+var navLinks=document.getElementById('navLinks')
+
+function showMenu(){
+    navLinks.style.right='0';
+}
+
+function hideMenu(){
+    navLinks.style.right='-200px';
+}
+
+function onlyNumberKey(evt) {
+          
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
+function detailssubmit() {
+    alert("Your details were Submitted");
+}
